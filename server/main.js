a18138ff7fd610755c859d42ed2cd35bffaff047
gvanhorne <gregory.vanhorne@gov.bc.ca>
2023-01-11 10:28:23 -0800

    Add user ownership to tasks

--- server/main.js
+++ server/main.js
@@ -2,7 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
 
-const insertTask = taskText => TasksCollection.insert({ text: taskText });
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
 
 const SEED_USERNAME = 'meteorite';
 const SEED_PASSWORD = 'password';
@@ -14,6 +19,8 @@ Meteor.startup(() => {
       password: SEED_PASSWORD
     });
   }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
 
   if (TasksCollection.find().count() === 0) {
     [
