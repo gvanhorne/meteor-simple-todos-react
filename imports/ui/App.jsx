75f51399f60665f1bdbabb57016cc4b028b3ef57
gvanhorne <gregory.vanhorne@gov.bc.ca>
2023-01-11 09:08:38 -0800

    Initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { Task } from './Task';
+import { TaskForm } from './TaskForm';
+
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+      <TaskForm/>
+
+      <ul>
+        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+      </ul>
+    </div>
+  )
+};
